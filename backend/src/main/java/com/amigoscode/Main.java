7088d2dc450f2bb9708eddef364d0e402f778c83
Spartak <spartaklysman@gmail.com>
2024-08-16 13:38:43 +0200

    CORS, Axios, Fetch Customers & Card Component

--- backend/src/main/java/com/amigoscode/Main.java
+++ backend/src/main/java/com/amigoscode/Main.java
@@ -9,8 +9,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
-import java.util.Locale;
 import java.util.Random;
 
 @SpringBootApplication
---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,59 @@
+import {Wrap, WrapItem, Spinner, Text} from "@chakra-ui/react";
+import SidebarWithHeader from "./components/shared/SideBar.jsx";
+import {useEffect, useState} from "react";
+import {getCustomers} from "./services/client.js";
+import CardWithImage from "./components/Card.jsx";
+
+const App = () => {
+
+    const [customers, setCustomers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+
+    useEffect(() => {
+        setLoading(true);
+        getCustomers().then(res => {
+            setCustomers(res.data)
+        }).catch(err => {
+            console.log(err)
+        }).finally(() => {
+            setLoading(false)
+        })
+    }, [])
+
+    if (loading) {
+        return (
+            <SidebarWithHeader>
+                <Spinner
+                    thickness='4px'
+                    speed='0.65'
+                    emptyColor='gray.200'
+                    color='blue.500'
+                    size='xl'
+                />
+            </SidebarWithHeader>
+        )
+    }
+
+    if (customers.length <= 0) {
+        return (
+            <SidebarWithHeader>
+                <Text>No customers available</Text>
+            </SidebarWithHeader>
+        )
+    }
+
+    return (
+        <SidebarWithHeader>
+            <Wrap justify={"center"} spacing={"30px"}>
+                {customers.map((customer, index) => (
+                    <WrapItem key={index}>
+                        <CardWithImage {...customer} />
+                    </WrapItem>
+                ))}
+            </Wrap>
+        </SidebarWithHeader>
+    )
+}
+
+export default App;
