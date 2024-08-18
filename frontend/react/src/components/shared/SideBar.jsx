f6554099ac8e07389557d275a6c94cfafadfdfd2
Spartak <spartaklysman@gmail.com>
2024-08-18 16:40:13 +0200

    New logo was edited

--- frontend/react/src/components/shared/SideBar.jsx
+++ frontend/react/src/components/shared/SideBar.jsx
@@ -87,8 +87,8 @@ const SidebarContent = ({onClose, ...rest}) => {
                 </Text>
                 <Image
                     borderRadius='full'
-                    boxSize='75px'
-                    src='https://user-images.githubusercontent.com/40702606/210880158-e7d698c2-b19a-4057-b415-09f48a746753.png'
+                    boxSize='110px'
+                    src='https://raw.githubusercontent.com/SpartakLysman/Spring-Fullstack-App/main/logo.png'
                     alt='Amigoscode'
                 />
                 <CloseButton display={{base: 'flex', md: 'none'}} onClick={onClose}/>
